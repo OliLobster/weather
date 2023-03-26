aa6572346e3103185fe8f1b1de08bb3555b6aa11
OliLobster <sgyr1993@163.com>
2023-03-26 19:32:12 +0800

    Update ShowWeather.js

--- src/components/ShowWeather.js
+++ src/components/ShowWeather.js
@@ -1,29 +1,45 @@
-import React from "react"
+import React from "react";
 
-const ShowWeather = () =>{
+const ShowWeather = ({ data }) => {
+
+    const city = data.name;
+    const country = data.sys ? data.sys.country : null;
+    const temperature = data.main ? data.main.temp : null;
+    const pressure = data.main ? data.main.pressure : null;
+    const visibility =  data ? data.visibility : null;
+    const humidity = data.main ? data.main.humidity : null;
+    const clouds =  data.clouds ? data.clouds.all : null;
+
+    // Values in standard units
+
+    const pressureInAtm = (pressure / 1000).toFixed(2);
+    const tempInCelcius = (temperature / 10).toFixed(2);
+    const visibilityInKM = (visibility / 1000).toFixed(2);
+
+
     return (
         <React.Fragment>
             <div className="showWeather">
-                <div className="weather_main">
+                <div className="weather_main" >
                     <h1 className="weather_heading">
-                        Seattle<br /> <span> AR</span>
+                        {city} <br /> <span> {country}</span>
                     </h1>
-                    <h3 className="temp">Temperature: 25C</h3>
+                    <h3 className="temp">Temperature: {tempInCelcius} C</h3>
                     <hr />
                     <div className="weatherData">
                         <p>
-                            Pressure <br /> 10 atm
+                            Pressure <br /> {pressureInAtm} atm{" "}
                         </p>
                         <p>
-                            Visibility <br /> 5 Km
+                            Visibility <br /> {visibilityInKM} Km
                         </p>
                     </div>
                     <div className="weatherData">
                         <p>
-                            Humidity: <br /> 80 %
+                            Humidity: <br /> {humidity}%{" "}
                         </p>
                         <p>
-                            Clouds: <br /> 10 %
+                            Clouds: <br /> {clouds} %{" "}
                         </p>
                     </div>
                 </div>
@@ -32,4 +48,4 @@ const ShowWeather = () =>{
     );
 };
 
-export default ShowWeather
+export default ShowWeather;
